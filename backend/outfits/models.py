246ab9c5237b36ca0c80cb930d9ad305dc2be6fc
Aksungkar <aksunkarmyktybek@gmail.com>
2026-06-03 00:19:31 +0500

    Deleted outfitItem, added list of items to Outfit

--- backend/outfits/models.py
+++ backend/outfits/models.py
@@ -5,14 +5,10 @@ from wardrobe.models import WardrobeItem
 
 class Outfit(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    outfit_items = models.ManyToManyField(WardrobeItem)
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
-    
-class OutfitItem(models.Model):
-    outfit = models.ForeignKey(Outfit, on_delete=models.CASCADE)
-    wardrobe_item = models.ForeignKey(WardrobeItem, on_delete=models.CASCADE)
-    
+
