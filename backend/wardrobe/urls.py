405442a34caa66d46e393ae8cc663faa6b6ed532
Aksungkar <aksunkarmyktybek@gmail.com>
2026-06-04 00:32:28 +0500

    Completed CRUD api, urls of Wardrobe

--- backend/wardrobe/urls.py
+++ backend/wardrobe/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import WardrobeListView
+from .views import WardrobeListView, WardrobeDetailView
 
 urlpatterns = [
-    path('', WardrobeListView.as_view()) 
+    path('', WardrobeListView.as_view()),
+    path('<int:id>', WardrobeDetailView.as_view())
 ]
--- backend/wardrobe/views.py
+++ backend/wardrobe/views.py
@@ -1,5 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from .serializers import WardrobeItemSerializer
 from .models import WardrobeItem
@@ -11,4 +13,37 @@ class WardrobeListView(APIView):
         return Response(serializer.data)
     
     
+    def post(self, request):
+        serializer = WardrobeItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class WardrobeDetailView(APIView):
+    def get_object(self, id):
+        # item = WardrobeItem.objects.get(pk=id) simple way, might be fall server, if no such id
+        # item = WardrobeItem.objects.get(pk=id, user=request.user) userге қарап жұмыс істеу
+        return get_object_or_404(WardrobeItem, pk=id)
+    
+    def get(self, request, id):
+        item = self.get_object(id)
+        serializer = WardrobeItemSerializer(item)
+        return Response(serializer.data)
+    
+    def put(self, request, id):
+        item = self.get_object(id)
+        serializer = WardrobeItemSerializer(item, data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, id):
+        item = self.get_object(id)
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
     
