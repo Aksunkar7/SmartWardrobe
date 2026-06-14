fd9429da581e6ef753ecb476b81dfa1a4b098760
Aksungkar <aksunkarmyktybek@gmail.com>
2026-06-14 17:26:50 +0500

    feat: created ng project, structured and added auth, token services

---
+++ smartwardrobe-frontend/src/app/core/services/auth.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { Token } from './token';
+
+interface AuthTokens {
+  access: string;
+  refresh: string;
+}
+
+interface RegisterData {
+  username: string;
+  email: string;
+  password: string;
+}
+
+interface LoginData {
+  username: string;
+  password: string
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private readonly API = 'http://localhost:8000/api/auth';
+
+  constructor (
+    private http: HttpClient,
+    private tokenService: Token,
+  ) {}
+
+  register(data: RegisterData): Observable<any> {
+    return this.http.post(`${this.API}/register/`, data)
+  }
+
+  login(data: LoginData): Observable<AuthTokens> {
+    return this.http.post<AuthTokens>(`${this.API}/token/`, data).pipe(
+      tap(tokens => {
+        this.tokenService.save(tokens.access, tokens.refresh);
+      })
+    )
+  }
+
+  refresh(): Observable<AuthTokens> {
+    const refreshToken = this.tokenService.getRefresh();
+    return this.http.post<AuthTokens>(`${this.API}/token/refresh/`, {
+      refresh: refreshToken
+    }).pipe(
+      tap(tokens => {
+        this.tokenService.save(tokens.access, tokens.refresh);
+      })
+    )
+  }
+
+  getMe(): Observable<any> {
+    return this.http.get(`${this.API}/me/`);
+  }
+
+  logout(): void {
+    return this.tokenService.clear();
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.tokenService.getAccess;
+  }
+}
---
+++ smartwardrobe-frontend/src/app/core/services/token.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Token {
+  
+  private readonly ACCESS_KEY = 'access_token';
+  private readonly REFRESH_KEY = 'refresh_token';
+
+  save(access: string, refresh: string): void {
+    localStorage.setItem(this.ACCESS_KEY, access);
+    localStorage.setItem(this.REFRESH_KEY, refresh);
+  }
+  
+  getAccess(): string | null {
+    return this.ACCESS_KEY;
+  }
+
+  getRefresh(): string | null {
+    return this.REFRESH_KEY;
+  }
+
+  clear(): void {
+    localStorage.removeItem(this.ACCESS_KEY);
+    localStorage.removeItem(this.REFRESH_KEY);
+  }
+}
