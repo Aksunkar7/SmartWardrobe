271dc86defb71900fd25580be312725e1c7ff963
Aksungkar <aksunkarmyktybek@gmail.com>
2026-06-15 19:07:31 +0500

    feat: added jwt-interceptor

---
+++ smartwardrobe-frontend/src/app/core/interceptors/jwt-interceptor.ts
@@ -0,0 +1,22 @@
+import { HttpInterceptorFn, HttpRequest, HttpHandlerFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Token } from '../services/token';
+
+export const jwtInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) => {
+  const tokenService = inject(Token);
+  const token = tokenService.getAccess();
+
+  if (token) {
+    const clonedReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(clonedReq);
+  }
+
+  return next(req);
+};
