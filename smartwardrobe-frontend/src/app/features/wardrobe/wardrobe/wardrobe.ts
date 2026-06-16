0e58ffe3d17cab620c8172ef76f1ac6dbb0d6961
Aksungkar <aksunkarmyktybek@gmail.com>
2026-06-16 19:34:03 +0500

    feat: added login, register, wardrobe components

---
+++ smartwardrobe-frontend/src/app/features/wardrobe/wardrobe/wardrobe.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wardrobe',
+  imports: [],
+  templateUrl: './wardrobe.html',
+  styleUrl: './wardrobe.scss',
+})
+export class Wardrobe {
+
+}
