ed8dbb37421dc53600b2c72e45a400becf04fe1a
Aksungkar <aksunkarmyktybek@gmail.com>
2026-06-18 15:39:36 +0500

    login form ready, works with backend

--- smartwardrobe-frontend/src/app/features/auth/login/login.ts
+++ smartwardrobe-frontend/src/app/features/auth/login/login.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Auth } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+
+  loginForm: FormGroup;
+  errorMessage: string = '';
+  isLoading: boolean = false;
+
+  constructor (
+    private fb: FormBuilder,
+    private authService: Auth,
+    private router: Router
+  ){
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) return;
+
+    this.isLoading = true;
+    this.errorMessage = '';
+    this.authService.login(this.loginForm.value).subscribe({
+      next: () => {
+        this.router.navigate(['/wardrobe'])
+      },
+      error: (err) => {
+        this.errorMessage = 'invalid login or password';
+        this.isLoading = false;
+      }
+    })
+  }
 
 }
