84f65ffd2b49981fd2f214bd534497b20304f797
Aksungkar <aksunkarmyktybek@gmail.com>
2026-08-03 22:21:36 +0500

    chore: add Celery worker with Redis broker/result backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -160,6 +160,12 @@ CACHES = {
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
-        "TIMEOUT": 3600,
+        "TIMEOUT": 3600, 
     }
 }
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
